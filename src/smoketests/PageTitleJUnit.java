834377e68b770d435fde9910ed27e854f8324524
Jdavidso1 <jdavidso1@hotmail.com>
2021-02-11 11:47:19 -0500

    refactored smoke test

--- src/smoketests/PageTitleJUnit.java
+++ src/smoketests/PageTitleJUnit.java
@@ -1,17 +1,35 @@
 package smoketests;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
 public class PageTitleJUnit {
+	WebDriver driver;
+	String webURL = "http://automationpractice.com/index.php?controller=authentication&back=my-account"; 
 	
 	@Test
 	public void pageTitleTest() {
-		String webURL = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
-		
-		WebDriver driver = utilities.DriverFactory.open("chrome");
+		System.out.println("Running the test");
 		driver.get(webURL);
+		
 		String actualTitle = driver.getTitle();
 		String expectedTitle = "Login - My Store";
+		Assert.assertEquals(expectedTitle, actualTitle);
+		// Assert.fail("We intentionally failed this test.");
+	}
+	
+	@Before
+	public void setUp() {
+		System.out.println("Initializing the driver");
+		driver = utilities.DriverFactory.open("chrome");
+	}
+	
+	@After
+	public void tearDown() {
+		System.out.println("Closing the driver");
+		driver.close();
 	}
 }
